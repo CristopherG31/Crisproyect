2ff4ca1621d3ae604ba19d0498e3c103c4e6c5ac
CrisDev <adriangonzalez1.uber@gmail.com>
2026-05-14 05:35:25 -0600

    se agrego enviorment

--- frontend/src/app/services/movie.ts
+++ frontend/src/app/services/movie.ts
@@ -1,43 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class MovieService {
 
-  private apiUrl = 'https://crisproyect.onrender.com/api/movies';
+  private apiUrl = `${environment.apiUrl}/movies`;
 
   constructor(private http: HttpClient) {}
 
-  // OBTENER PELÍCULAS
   getMovies(): Observable<any> {
-
     return this.http.get(this.apiUrl);
-
   }
 
-  // AGREGAR PELÍCULA
-  addMovie(movie:any): Observable<any> {
-
+  addMovie(movie: any): Observable<any> {
     return this.http.post(this.apiUrl, movie);
-
   }
 
-  // ACTUALIZAR PELÍCULA
-  updateMovie(id:number, movie:any): Observable<any> {
-
+  updateMovie(id: number, movie: any): Observable<any> {
     return this.http.put(`${this.apiUrl}/${id}`, movie);
-
   }
 
-  // ELIMINAR PELÍCULA
-  deleteMovie(id:number): Observable<any> {
-
+  deleteMovie(id: number): Observable<any> {
     return this.http.delete(`${this.apiUrl}/${id}`);
-
   }
-
 }
